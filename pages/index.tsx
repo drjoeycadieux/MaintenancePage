e2d0659795b796313c2dae1bf39705e1be4cfd4a
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-05-04 15:13:09 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Techtack-Technologies Under Maintenance</title>
+        <meta
+          name="description"
+          content="Techtack-Technologies Powered By TeamLitho"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div>
+          <p>Techtack-Technologies</p>
+          <span>
+            The site is under maintenance and comes back very quickly, thank you
+            for your patience.
+          </span>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
