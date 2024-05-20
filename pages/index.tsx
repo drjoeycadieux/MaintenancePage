4c91d8a1491d8420a69c75700a5a395d2c196222
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-05-20 21:13:13 +0000

    inm.

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div>
-          <p>Techtack-Technologies</p>
-          <span>
-            The site is under maintenance and comes back very quickly, thank you
-            for your patience.
-          </span>
-        </div>
+        <h1>Techtack-Technologies</h1>
+        <br/>
+        <p>This site is under maintenance come back very quickly, thank you for yourr patience.</p>
       </main>
-      <div className="inner"></div>
-      <div className="container">
-        <p>Idea. Software. Fast.</p>
-      </div>
     </div>
   );
 };
